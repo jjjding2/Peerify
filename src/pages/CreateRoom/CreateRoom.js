fbbf40e6563fab1293a8d0838dc6e745dc0cb92b
William Li <liwilgq@gmail.com>
2020-08-22 14:05:43 -0400

    added more stuff

--- src/pages/CreateRoom/CreateRoom.js
+++ src/pages/CreateRoom/CreateRoom.js
@@ -21,18 +21,35 @@ function genKey() {
     return genRandomNumber();
 }
 
+function trimText(str) {
+    while(str.length > 0 && str[0] == ' ') str = str.substring(1);
+    while(str.length > 0 && str[str.length-1] == ' ') str = str.substring(0,str.length-1);
+    return str;
+}
+
 class CreateRoom extends React.Component {
     constructor() {
         super();
 
         this.state = {
             roomID: genKey(),
+            numRounds: -1,
             currentPlayers: [],
         };
+
+        this.create = this.create.bind(this);
     }
 
-    componentDidMount() {
-
+    create() {
+        let owner_nick = trimText(document.getElementById('owner-nickname').value);
+        let num_rounds = document.getElementById('num-rounds').value;
+        if(owner_nick == "" || num_rounds == "") return;
+        let curr = this.state.currentPlayers;
+        curr.push(owner_nick);
+        this.setState({
+            numRounds: num_rounds,
+            currentPlayers: curr,
+        });
     }
 
     render() {
@@ -40,13 +57,24 @@ class CreateRoom extends React.Component {
         return (
             <div>
                 <Navbar />
-                <p style = {{ height: '6vh', position: 'fixed', left: '4vw', top: '8vh', fontSize: '3vh', fontFamily: 'OpenSans-Light' }}>
-                    { roomIDText }
-                </p>
-                <button className = 'start-button' style = {{ position: 'fixed', right: '4vw', top: '8vh', transform: 'translate(0,50%)' }}>
-                    Start Game
-                </button>
-                <JoinedPlayers roomID = { this.state.roomID } />
+                {
+                    this.state.numRounds == -1?
+                    <div className = 'enter-room-code'>
+                        <input id = 'owner-nickname' className = 'enter-code-box' placeholder = 'Nickname' />
+                        <input id = 'num-rounds' className = 'enter-code-box' style = {{ top: '50%' }} type = 'number' placeholder = '# Rounds' />
+                        <button className = 'confirm-join-button' style = {{ top: '100%' }} onClick = { this.create }> Create </button>
+                    </div>
+                    :
+                    <div>
+                        <p style = {{ height: '6vh', position: 'fixed', left: '4vw', top: '8vh', fontSize: '3vh', fontFamily: 'OpenSans-Light' }}>
+                            { roomIDText }
+                        </p>
+                        <button className = 'start-button' style = {{ position: 'fixed', right: '4vw', top: '8vh', transform: 'translate(0,50%)' }}>
+                            Start Game
+                        </button>
+                        <JoinedPlayers roomID = { this.state.roomID } />
+                    </div>
+                }
             </div>
         );
     }
---
+++ src/pages/Play/Play.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Play extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLeader: false,
+            prompt: "",
+            startTime: 0,
+            endTime: 0,
+        };
+    }
+
+    render() {
+        return (
+            null
+        );
+    }
+}
