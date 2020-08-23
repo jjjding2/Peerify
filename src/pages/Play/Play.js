94e10e58ddf79a736a5aba0598ca45d03ac71bed
Andrew Xue <andewzxue@gmail.com>
2020-08-23 17:31:54 -0400

    ok

--- src/pages/Play/Play.js
+++ src/pages/Play/Play.js
@@ -5,6 +5,9 @@ import Waiting from '../../components/Waiting/Waiting';
 
 import socket from '../../socket';
 
+import steppingUp from '../../images/steppingUp.png';
+import books from '../../images/books.png';
+
 import './Play.css';
 
 function trimText(str) {
@@ -33,8 +36,27 @@ class Play extends React.Component {
             doneScoreboard: false,
             gameOver: false,
 
-            topUsers: [],
-            userEntries: [],
+            topUsers: [
+                {
+                    name: '1',
+                    score: 10,
+                }, {
+                    name: '2',
+                    score: 10,
+                }, {
+                    name: '3',
+                    score: 10,
+                }, {
+                    name: '4',
+                    score: 10,
+                }, {
+                    name: '5',
+                    score: 10,
+                }
+            ],
+            userEntries: [
+
+            ],
 
             rating: 2.5,
         };
@@ -45,15 +67,6 @@ class Play extends React.Component {
             socket.emit('promptStage', this.state.roomID);
         }
 
-        socket.on('finishPrompt', () => {
-            if (this.state.userID == this.state.leader) {
-                if (!this.state.promptSubmitted){
-                    this.setState({ promptSubmitted: true });
-                    socket.emit('writingStage', this.state.roomID, document.getElementById('prompt').value);
-                }
-            }
-        });
-
         socket.on('prompt', prompt => {
             this.setState({
                 prompt,
@@ -61,45 +74,20 @@ class Play extends React.Component {
             });
         });
 
-        socket.on('finishWriting', () => {
-            if (!this.state.entrySubmitted) {
-                this.setState({ entrySubmitted: true });
-                socket.emit('sendText', this.state.userID, this.state.roomID, document.getElementById('user-response').value);
-            }
-            this.setState({ allEntrySubmitted: true });
-        });
-
         socket.on('allSubmitted', () => {
             this.setState({ allEntrySubmitted: true });
             socket.emit('getEvaluation', this.state.userID, this.state.roomID);
-            if(this.state.userID == this.state.leader){
-                socket.emit('evaluationStage', this.state.RoomID);
-            }
         });
 
         socket.on('evaluation', evalEntry => {
             console.log("eval entry "+evalEntry);
             this.setState({ evalEntry });
-            if (this.state.userID == this.state.leader) {
-                socket.emit('evaluationStage', this.state.roomID);
-            }
-        });
-
-        socket.on('finishEvaluation', () => {
-            if (!this.state.evalSubmitted){
-                this.setState({ evalSubmitted: true });
-                socket.emit('sendEvaluation', this.state.userID, this.state.roomID, document.getElementById('user-evaluation').value, this.state.rating)
-            }
-            // this.setState({ allEvalSubmitted: true });
         });
 
         socket.on('allEvaluated', () => {
             // this.setState({ allEvalSubmitted: true });
             socket.emit('getFeedback', this.state.userID, this.state.roomID);
             socket.emit('getScore', this.state.userID, this.state.roomID);
-            if(this.state.userID == this.state.leader){
-                socket.emit('feedbackStage', this.state.roomID);
-            }
         });
 
         socket.on('feedback', feedback => {
@@ -121,18 +109,6 @@ class Play extends React.Component {
             this.setState({ gameOver: true });
         });
 
-        socket.on('finishFeedback', () => {
-            console.log("FEEDBACK FINISHED");
-            if(!this.state.doneFeedback){
-                this.setState({ allDoneFeedback: true, doneFeedback: true });
-            }
-            if (this.state.userID == this.state.leader) {
-                socket.emit('scoreboardStage', this.state.roomID);
-                socket.emit('getResults', this.state.roomID, 5);
-                socket.emit('getEntries', this.state.roomID);
-            }
-        });
-
         socket.on('results', array => {
             console.log("RESULTS GOTTEN");
             this.setState({ topUsers: array });
@@ -146,7 +122,6 @@ class Play extends React.Component {
             console.log("FEEDBACK DONE");
             this.setState({ allDoneFeedback: true, doneFeedback: true });
             if (this.state.userID == this.state.leader){
-                socket.emit('scoreboardStage', this.state.roomID);
                 socket.emit('getResults', this.state.roomID, 5);
                 socket.emit('getEntries', this.state.roomID);
             }
@@ -301,35 +276,37 @@ class Play extends React.Component {
                                     if (!this.state.doneScoreboard){
                                         component = 
                                         <div style={{ textAlign: 'center' }}>
-                                            <h1 className = 'page-header'>Current Scoreboard</h1>
-                                            <div style={{ display: 'flex', width: '100%', marginTop: '120px', justifyContent: 'center'}}>
+                                            <h1 className = 'page-header' style={{ marginTop: '100px', fontSize: '60px' }}>Current Scoreboard</h1>
+                                            <div style={{ display: 'flex', width: '100%', height: '100vh', marginTop: '300px', justifyContent: 'center'}}>
+                                            <img src={steppingUp} style={{ width: '500px', height: '380px' }}></img>
                                             <div>
-                                                <h2>Name</h2>
+                                                <h2 className='scoreboard'>Name</h2>
                                                 {
                                                 this.state.topUsers.map(function(user, idx) {
                                                     if (user.name != ""){
                                                         return (
-                                                            <h3>{user.name}</h3>
+                                                            <h3 className='scoreboard'>{user.name}</h3>
                                                         )
                                                     }
                                                 })
                                             }
                                             </div>
-                                            <div style={{marginLeft: '100px'}}>
-                                            <h2>Score</h2>
+                                            <div style={{marginLeft: '300px'}}>
+                                            <h2 className='scoreboard'>Score</h2>
                                             {
                                                 this.state.topUsers.map(function(user, idx) {
                                                     if (user.name != ""){
                                                         return (
-                                                            <h3>{user.score}</h3>
+                                                            <h3 className='scoreboard'>{user.score}</h3>
                                                         )
                                                     }
                                                 })
                                             }
                                             </div>
+                                            <img src={books} style={{ width: '500px', height: '266px' }}></img>
                                             </div>
-                                            <h2 style={{ marginTop: '100px' }}> Entries </h2>
-                                            <div style={{ display: 'flex', width: '100%', justifyContent: 'center'}}>
+                                            <h2> Entries </h2>
+                                            <div style={{ display: 'flex', width: '100%', justifyContent: 'center', marginBottom: '500px'}}>
                                             {
                                                 this.state.userEntries.map(function(entry, idx) {
                                                     return (
