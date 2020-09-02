5deebbf9a94fba0046cff78a2beb63572ccd618b
William Li <liwilgq@gmail.com>
2020-09-02 19:58:59 -0400

    updated socket endpoint

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import socketIOClient from "socket.io-client";
 
-const ENDPOINT = "http://138.197.129.190:5000";
+const ENDPOINT = "http://159.89.114.63:5000";
 // const ENDPOINT = "http://localhost:5000";
 
 const socket = socketIOClient(ENDPOINT);
