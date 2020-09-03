63c9d3d24dffb2a406c5749b9d252ed4beb3268d
William Li <liwilgq@gmail.com>
2020-09-02 20:01:04 -0400

    changed to https

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import socketIOClient from "socket.io-client";
 
-const ENDPOINT = "http://159.89.114.63:5000";
+const ENDPOINT = "https://159.89.114.63:5000";
 // const ENDPOINT = "http://localhost:5000";
 
 const socket = socketIOClient(ENDPOINT);
